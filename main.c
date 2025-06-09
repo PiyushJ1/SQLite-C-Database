ada7888c059907bd519ea148ce0e02f42e95bcbb
Piyush <piyxsh@gmail.com>
2025-06-09 19:35:37 +1000

    wrote a manual implementation of the getline() function

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <stdbool.h>
+#include <string.h>
 
 // wrapper struct to interact with the getline() function
 typedef struct {
@@ -10,15 +11,16 @@ typedef struct {
     ssize_t inputLen;
 } InputBuffer;
 
-void printPrompt();
 InputBuffer *newInputBuffer();
+void printPrompt();
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+void readInput(InputBuffer *inputBuff);
 
 int main(int argc, char *argv[]) {
     InputBuffer *inputBuff = newInputBuffer();
 
     while (true) {
         printPrompt();
-        
     }
 
 
@@ -26,9 +28,6 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void printPrompt() {
-    printf("db > ");
-}
 
 // initialises new input buffer
 InputBuffer *newInputBuffer() {
@@ -36,7 +35,64 @@ InputBuffer *newInputBuffer() {
     inputBuff->buffer = NULL;
     inputBuff->bufferLen = 0;
     inputBuff->inputLen = 0;
-
+    
     return inputBuff;
 }
 
+void printPrompt() {
+    printf("db > ");
+}
+
+/**
+ * Manual implementation of the getline() function, which isn't available on
+ * C99 on Windows machines (without additional configuration since it is a 
+ * POSIX function).
+ */
+ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
+    if (lineptr == NULL || n == NULL || stream == NULL) return -1;
+
+    // initial buffer size and char position of input
+    size_t buffSize = 128;
+    size_t pos = 0;
+    int c;
+
+    if (*lineptr == NULL) {
+        // allocate memory for lineptr if NULL
+        *lineptr = malloc(buffSize);
+        if (*lineptr == NULL) {
+            fprintf(stderr, "error: could not allocate memory for getline()\n");
+            return -1;
+        }
+        
+        *n = buffSize;
+    }
+
+    while ((c = fgetc(stream)) != EOF) {
+        // double the memory size and allocate more memory
+        if (pos + 1 >= *n) {
+            size_t newSize = *n * 2;
+            char *newPtr = realloc(*lineptr, newSize);
+            if (newPtr == NULL) {
+                fprintf(stderr, "error: could not allocate memory for getline()\n");
+                return -1;
+            }
+            *lineptr = newPtr;
+            *n = newSize;
+        }
+
+        (*lineptr)[pos] = (char) c;
+        pos++;
+
+        if (c == '\n') break;
+    }
+
+    if (pos == 0 && c == EOF) return -1;
+
+    (*lineptr)[pos] = '\0';
+
+    return pos;
+}
+
+void readInput(InputBuffer *InputBuff) {
+
+}
